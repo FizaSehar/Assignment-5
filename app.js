489497e8bc05fe7b9faa07e404487514522bd33d
FizaSehar <fizasehar783@gmail.com>
2020-07-11 14:52:58 +0500

    Assignment 5

---
+++ app.js
@@ -0,0 +1,17 @@
+//chap  # 1 task # 1
+alert('Assalam Alaikum....')
+
+//task # 2
+alert(' Error! Please enter a valid password.')
+
+//task # 3
+alert(' Welcome to JS Land...... \n Happy Coding !')
+
+//task # 4
+alert(' Welcome to JS Land....') 
+alert('Happy Coding !')
+
+
+ //task # 5
+ alert('Hello....I can run JS through my web browser’s console')
+
